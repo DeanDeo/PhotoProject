bf220ad47aa41f7abf861ce6f781697d8afa59df
May Z <maydeyn@gmail.com>
2019-01-25 15:15:29 -0600

    updated to react

--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -1,15 +1,40 @@
 import React from "react";
 import "./landing.css";
-// import bokehLogo from "../../../public/images/logo.png";
-// import "../../../public/images/photographer-1.jpg";
-// import "../../../public/images/vino.jpg";
 
 class LandingPage extends React.Component {
   //   constructor(props) {
   //     super(props);
   //   }
   render() {
-    return <h1>HI</h1>;
+    return (
+      <div className="row">
+        <div className="split-pane hvr-shutter-out-vertical-white col-sm-6 client-side">
+          <div>
+            <i className="far fa-user-circle font-black" /> <br />
+            <br />
+            <div className="text-content font-black">
+              <div className="big font-black">HIRE A</div>
+              <div className="bold font-black">PHOTOGRAPHER</div>
+            </div>
+            <button>Log In with Google</button>
+          </div>
+        </div>
+        <div className="split-pane hvr-shutter-out-vertical col-sm-6 photographer-side">
+          <div>
+            <i className="fas fa-camera" /> <br />
+            <br />
+            <div className="text-content">
+              <div className="big">I AM A</div>
+              <div className="bold">PHOTOGRAPHER</div>
+            </div>
+            <a className="button">Log in with Google </a>
+          </div>
+        </div>
+      </div>
+      // <div id="split-pane-or">
+      // <div><img src={require('./logo.png')} /></div>
+      // </div>
+    );
   }
 }
 
