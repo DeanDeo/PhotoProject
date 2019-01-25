44f98b5ed3db68594b9c29e398e8e746f0b481f2
pholmes10 <pholmes10@gmail.com>
2019-01-25 13:30:09 -0600

    added landingPage

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,14 @@ import NoMatch from "./pages/NoMatch";
 import UserProfile from "./pages/UserProfile";
 import PhotogProfile from "./pages/PhotogProfile";
 import Results from "./pages/Results"
+import LandingPage from "./pages/LandingPage";
 
 const App = () => (
   <Router>
     <div>
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={LandingPage} />
+        <Route exact path="/home" component={Home} />
         <Route exact path="/userProfile" component={UserProfile} />
         <Route exact path="/photogProfile" component={PhotogProfile} />
         <Route exact path="/results" component={Results} />
---
+++ client/src/pages/LandingPage/LandingPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class LandingPage extends React.Component {
+  //   constructor(props) {
+  //     super(props);
+  //   }
+  render() {
+    return (
+            <div>
+                <h1>This is our Landing Page</h1>
+            </div>
+            );
+          }
+        }
+        
+export default LandingPage;
