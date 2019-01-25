e51b508a16621d2d04e5c10f9b2845cba56552e7
May Z <maydeyn@gmail.com>
2019-01-25 14:13:41 -0600

    added landing page

--- client/src/pages/LandingPage/LandingPage.js
+++ client/src/pages/LandingPage/LandingPage.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
+import "./landing.css";
+// import bokehLogo from "../../../public/images/logo.png";
+// import "../../../public/images/photographer-1.jpg";
+// import "../../../public/images/vino.jpg";
 
 class LandingPage extends React.Component {
   //   constructor(props) {
   //     super(props);
   //   }
   render() {
-    return (
-            <div>
-                <h1>This is our Landing Page</h1>
-            </div>
-            );
-          }
-        }
-        
+    return <h1>HI</h1>;
+  }
+}
+
 export default LandingPage;
