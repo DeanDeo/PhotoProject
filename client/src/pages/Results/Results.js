6f4dd66e21ae6349b6ebe0aa92dd8de1f804d8d0
pholmes10 <pholmes10@gmail.com>
2019-01-27 22:08:07 -0600

    tiny bit of grid css for results page

--- client/src/pages/Results/Results.js
+++ client/src/pages/Results/Results.js
@@ -1,19 +1,47 @@
 import React from 'react';
 import ResultBox from '../../components/ResultBox';
+import './Results.css';
 
 class Results extends React.Component {
     // constructor(props) {
     //   super(props);
     // }
   render() {
-    const myStyles = {
-      results: {
-         display: 'grid',
-      }
-  }
+
     return (
-            <div style={myStyles.results}>
+            <div className='results'>
                 <h1>This is our Results Page</h1>
+                <ResultBox></ResultBox>
+                <ResultBox></ResultBox>
+                <ResultBox></ResultBox>
+                <ResultBox></ResultBox>
+                <ResultBox></ResultBox>
+                <ResultBox></ResultBox>
+                <ResultBox></ResultBox>
+                <ResultBox></ResultBox>
+                <ResultBox></ResultBox>
+                <ResultBox></ResultBox>
+                <ResultBox></ResultBox>
+                <ResultBox></ResultBox>
+                <ResultBox></ResultBox>
+                <ResultBox></ResultBox>
+                <ResultBox></ResultBox>
+                <ResultBox></ResultBox>
+                <ResultBox></ResultBox>
+                <ResultBox></ResultBox>
+                <ResultBox></ResultBox>
+                <ResultBox></ResultBox>
+                <ResultBox></ResultBox>
+                <ResultBox></ResultBox>
+                <ResultBox></ResultBox>
+                <ResultBox></ResultBox>
+                <ResultBox></ResultBox>
+                <ResultBox></ResultBox>
+                <ResultBox></ResultBox>
+                <ResultBox></ResultBox>
+                <ResultBox></ResultBox>
+                <ResultBox></ResultBox>
+                <ResultBox></ResultBox>
                 <ResultBox></ResultBox>
                 <ResultBox></ResultBox>
                 <ResultBox></ResultBox>
