9747625acf8c8557cf7eba3090d5dff10fdce4bf
pholmes10 <pholmes10@gmail.com>
2019-01-29 14:27:14 -0600

    api key

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,7 +1,7 @@
 import firebase from "firebase";
 
 const config = {
-  apiKey: process.env.REACT_APP_FIREBASE_API,
+  apiKey: 'AIzaSyC5L8USzVlO_kH6NvhxBmi5ECLWKNiTxRM',
   authDomain: "photoapp-92661.firebaseapp.com",
   databaseURL: "https://photoapp-92661.firebaseio.com"
 };
