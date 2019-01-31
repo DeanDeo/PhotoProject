2fb04676415096b447acb987fb9d0a14d54cb896
Nate <nathanw88@gmail.com>
2019-01-31 15:00:05 -0600

    photos heroku

--- server.js
+++ server.js
@@ -4,48 +4,13 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const multer  = require('multer');
-const fs = require("fs")
-
-const data = 
-  {
-    type: process.env.type,
-    project_id: process.env.project_id,
-    private_key_id: process.env.private_key_id,
-    private_key: process.env.private_key,
-    client_email: process.env.client_email,
-    client_id: process.env.client_id,
-    auth_uri: process.env.auth_uri,
-    token_uri: process.env.token_uri,
-    auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
-    client_x509_cert_url: process.env.client_x509_cert_url
-  }
-
-  try {
-    if (fs.existsSync("./final-project-230222-5ae4963fbec8.json")) {
-    
-    }
-
-else{
-  fs.writeFile ("final-project-230222-5ae4963fbec8.json", JSON.stringify(data), function(err) {
-    if (err) throw err;
-    console.log('complete');
-  });
-
-}
-} catch(err) {
-  console.error(err)
-}
 
 
 
 
 
 
-// fs.writeFile ("final-project-230222-5ae4963fbec8.json", JSON.stringify(data), function(err) {
-//   if (err) throw err;
-//   console.log('complete');
-//   }
-// );
+
 
 const PORT = process.env.PORT || 3001;
 
