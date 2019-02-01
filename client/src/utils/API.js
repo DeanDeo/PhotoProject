af5719aa5c54d692f08b30004048015f7efb7124
pholmes10 <pholmes10@gmail.com>
2019-02-01 12:10:47 -0600

    search done

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,28 +1,28 @@
 import axios from "axios";
 
 export default {
-  getCustomers: function() {
+  getCustomers: function () {
     return axios.get("/api/customers");
   },
-  getCustomer: function(id) {
+  getCustomer: function (id) {
     return axios.get("/api/customers/" + id);
   },
-  saveCustomer: function(customerData, id) {
-    return axios.post("/api/customers/edit/" +id, customerData);
+  saveCustomer: function (customerData, id) {
+    return axios.post("/api/customers/edit/" + id, customerData);
   },
   // deleteCustomer: function(id) {
   //   return axios.delete("/api/customers/" + id);
   // },
 
-  getPhotographers: function() {
-    return axios.get("/api/photographers");
+  getPhotographers: function (location) {
+    return axios.post("/api/photographers", location);
   },
-  getPhotographer: function(id) {
+  getPhotographer: function (id) {
     return axios.get("/api/photographers/" + id);
   },
-  savePhotographer: function(photographerData, id ) {
-    
-    return axios.post("/api/photographers/edit/" +id, photographerData);
+  savePhotographer: function (photographerData, id) {
+
+    return axios.post("/api/photographers/edit/" + id, photographerData);
   }
   // deletePhotographer: function(id) {
   //   return axios.delete("/api/photographers/" + id);
--- controllers/photoController.js
+++ controllers/photoController.js
@@ -6,49 +6,52 @@ module.exports = {
 
   create: (req, res) => {
 
-  
+
     db.Photo.create(req)
       .then(result => {
-        
+
       })
       .catch(err => console.log(err));
   },
-  findbyid:(req, res) => {
-    db.Photo.findById(req.body.id).then(result =>{}).catch(err => res.status(422).json(err));
+  findbyid: (req, res) => {
+    db.Photo.findById(req.body.id).then(result => { }).catch(err => res.status(422).json(err));
   },
-  populatePhotos: (req, res)=>{
-    
-    db.Photo.find({photographer: req.params.id}).then(result=>{
-      res.json(result) 
+  populatePhotos: (req, res) => {
+
+    db.Photo.find({ photographer: req.params.id }).then(result => {
+      res.json(result)
     }).catch(err => res.status(422).json(err));
-    },
-    findAll: (req, res) =>{
-      db.Photo.find({}).then(result=>{
-        // console.log(result)
-        res.json(result)
-      }).catch(err => res.status(422).json(err));
-    },
-    findMain: (req, res) =>{
-      db.Photo.find({main_photo: true}).then(result=>{
-        // console.log(result)
-        res.json(result)
-      }).catch(err => res.status(422).json(err));
-    },
-    updateMain:(req, res) =>{
-      
-      
-      db.Photo.findOneAndUpdate({photographer: req.body.id, main_photo: true},{$set:{main_photo:false}}).then(result=>{
-        db.Photo.findOneAndUpdate({_id: req.body.picId},{$set:{main_photo:true}}).then(response=>{
-          
-          res.json(response)
-        })
-      }).catch(err => res.status(422).json(err));
-    },
-    updateProfile:(req, res) =>{
-      db.Photo.findOneAndUpdate({photographer: req.body.id, profile_photo: true},{$set:{profile_photo:false}}).then(result=>{
-        db.Photo.findOneAndUpdate({_id: req.body.picId},{$set:{profile_photo:true}}).then(response=>{
-          res.json(response)
-        })
-      }).catch(err => res.status(422).json(err));
-    }
+  },
+  findAll: (req, res) => {
+    db.Photo.find({}).then(result => {
+      // console.log(result)
+      res.json(result)
+    }).catch(err => res.status(422).json(err));
+  },
+  findMain: (req, res) => {
+    db.Photo.find({ main_photo: true }).then(result => {
+      // console.log(result)
+      res.json(result)
+    }).catch(err => res.status(422).json(err));
+  },
+  searchMainPhotos: (req, res) => {
+    db.Photo.find({ main_photo: true, photographer: req.params.id }).then(result => {
+      res.json(result)
+    }).catch(err => res.status(422).json(err))
+  },
+  updateMain: (req, res) => {
+    db.Photo.findOneAndUpdate({ photographer: req.body.id, main_photo: true }, { $set: { main_photo: false } }).then(result => {
+      db.Photo.findOneAndUpdate({ _id: req.body.picId }, { $set: { main_photo: true } }).then(response => {
+
+        res.json(response)
+      })
+    }).catch(err => res.status(422).json(err));
+  },
+  updateProfile: (req, res) => {
+    db.Photo.findOneAndUpdate({ photographer: req.body.id, profile_photo: true }, { $set: { profile_photo: false } }).then(result => {
+      db.Photo.findOneAndUpdate({ _id: req.body.picId }, { $set: { profile_photo: true } }).then(response => {
+        res.json(response)
+      })
+    }).catch(err => res.status(422).json(err));
+  }
 };
